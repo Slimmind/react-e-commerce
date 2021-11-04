48390ed03de6571ffec837aa1104f147d43418fe
Paul Siroshtan <slimmind@gmail.com>
2021-11-04 17:20:19 +0200

    updated shop data with new data, added cart utils to group items in cart.reducer

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -3,3 +3,8 @@ import { CartActionTypes } from "./cart.types";
 export const toggleCartDropdownVisibility = () => ({
   type: CartActionTypes.TOGGLE_CART_DROPDOWN_VISIBILITY
 });
+
+export const addCartItem = (item) => ({
+  type: CartActionTypes.ADD_ITEM,
+  payload: item
+});
