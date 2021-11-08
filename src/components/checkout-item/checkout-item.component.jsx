7e26684eedf21d60501e82f9c55095ea7933ed20
Paul Siroshtan <slimmind@gmail.com>
2021-11-08 16:38:11 +0200

    added addCartItem and removeCartItem functionality to checkout-item component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,20 +1,36 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { clearItemFromCart, addCartItem, removeCartItem } from '../../redux/cart/cart.actions';
 
 import './checkout-item.styles.scss';
 
 
-const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity} }) => (
-  <div className="checkout-item">
-    <div className="image-container">
-      <img src={imageUrl} alt={name} />
+const CheckoutItem = ({ cartItem, clearItem, addCartItem, removeCartItem}) => {
+  const { name, imageUrl, price, quantity } = cartItem;
+  return (
+    <div className="checkout-item">
+      <div className="image-container">
+        <img src={imageUrl} alt={name} />
+      </div>
+      <span className="name">{name}</span>
+      <div className="quantity">
+        <button className="arrow" onClick={() => removeCartItem(cartItem)}>&#10094;</button>
+        <span className="value">{quantity}</span>
+        <button className="arrow" onClick={() => addCartItem(cartItem)}>&#10095;</button>
+      </div>
+      <span className="price">{price}</span>
+      <div>
+        <button className="remove-button" onClick={() => clearItem(cartItem)}>&#10005;</button>
+      </div>
     </div>
-    <span className="name">{name}</span>
-    <span className="quantity">{quantity}</span>
-    <span className="price">{price}</span>
-    <div>
-      <button className="remove-button">&#10005;</button>
-    </div>
-  </div>
-);
+  )
+};
 
-export default CheckoutItem;
+const mapDispatchToProps = dispatch => ({
+  clearItem: item => dispatch(clearItemFromCart(item)),
+  addCartItem: item => dispatch(addCartItem(item)),
+  removeCartItem: item => dispatch(removeCartItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
